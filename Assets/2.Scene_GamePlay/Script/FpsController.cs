d4b3a784020132749423d2d45f96171e29afd7c9
King <alsghqk@gmail.com>
2021-05-07 00:58:59 +0900

    210507_PlayerStopUi
    1. StopUI 기능
    2. CommandUI 기능
    3. ESC, F1 키 입력 기능

--- Assets/2.Scene_GamePlay/Script/FpsController.cs
+++ Assets/2.Scene_GamePlay/Script/FpsController.cs
@@ -8,6 +8,9 @@ public class FpsController : MonoBehaviour
     public Rigidbody fpsRigidBody;
     public bool IsInvertYaxis = false; // 상하 반전 유무
     public float movementSpeed = 3;
+    public GameObject SetActiveUiCommand;
+    public GameObject SetActiveUiStop;
+    public bool UiCheck = false;
     
     // Start is called before the first frame update
     void Start()
@@ -18,42 +21,76 @@ public class FpsController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        var mouseX = Input.GetAxis("Mouse X"); // 마우스 X축(움직임) 값을 가져온다.
-        var mouseY = Input.GetAxis("Mouse Y"); // 마우스 y축(움직임) 값을 가져온다.
-
-        // print(message:$"mouse X: {mouseX}, mouse Y: {mouseY}"); // 마우스 변경 값을 출력한다.
-
-        // 좌, 우 화면 회전은 캐릭터 모델(몸체)의 카메라를 바꿔야한다.
-        transform.eulerAngles = transform.eulerAngles + new Vector3(0, mouseX, 0);  // x, y, z축의 각도 값을 넣어준다. // 화면을 회전하게 해줄 수 있게함.
-
-        // 위, 아래 화면 회전은 몸체 안에 있는 카메라를 바꿔야한다.
-        // var isInvert = -1;
-        // if(IsInvertYaxis == false) // 기본 값: 상하반전 X
-        // {
-        //     isInvert = -1;
-        // }
-        // else // IsInvertYaxis=true 값: 상하반전 O
-        // {
-        //     isInvert = 1;
-        // }
-        fpsCamera.transform.eulerAngles = fpsCamera.transform.eulerAngles + new Vector3(mouseY * (IsInvertYaxis? 1 : -1), 0, 0); // 조건식을 3항식으로 대체했음.
-
-        var keyboardX = Input.GetAxis("Horizontal"); // ws, 좌 우 버튼을 누르면 값을 받음
-        var keyboardY = Input.GetAxis("Vertical"); // sw, 위 아래 버튼을 누르면 값을 받음
-
-        // 앞으로 나가아는 속도는 KeyboardX의 값으로 한다. // Left Shift를 누를 경우 달린다.
-        if(Input.GetKey(KeyCode.LeftShift))
+    {       
+        if (Input.GetKeyDown(KeyCode.F1))
         {
-            movementSpeed = 6;
-            // print(movementSpeed);
+            if (!UiCheck)
+            {
+                UiCheck = true;
+                SetActiveUiCommand.SetActive(true);
+            }
+            else
+            {
+                UiCheck = false;
+                SetActiveUiCommand.SetActive(false);
+            }
         }
-        else
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            movementSpeed = 3;
-            // print(movementSpeed);
+            if (!UiCheck)
+            {
+                UiCheck = true;
+                SetActiveUiStop.SetActive(true);
+            }
+            else
+            {
+                UiCheck = false;
+                SetActiveUiStop.SetActive(false);
+            }
         }
-        fpsRigidBody.velocity = (transform.forward * (keyboardY * movementSpeed)) +
-                                (transform.right * (keyboardX * movementSpeed));
+
+        if (!UiCheck)
+        {
+            var mouseX = Input.GetAxis("Mouse X"); // 마우스 X축(움직임) 값을 가져온다.
+            var mouseY = Input.GetAxis("Mouse Y"); // 마우스 y축(움직임) 값을 가져온다.
+
+            // print(message:$"mouse X: {mouseX}, mouse Y: {mouseY}"); // 마우스 변경 값을 출력한다.
+
+            // 좌, 우 화면 회전은 캐릭터 모델(몸체)의 카메라를 바꿔야한다.
+            transform.eulerAngles =
+                transform.eulerAngles + new Vector3(0, mouseX, 0); // x, y, z축의 각도 값을 넣어준다. // 화면을 회전하게 해줄 수 있게함.
+
+            // 위, 아래 화면 회전은 몸체 안에 있는 카메라를 바꿔야한다.
+            // var isInvert = -1;
+            // if(IsInvertYaxis == false) // 기본 값: 상하반전 X
+            // {
+            //     isInvert = -1;
+            // }
+            // else // IsInvertYaxis=true 값: 상하반전 O
+            // {
+            //     isInvert = 1;
+            // }
+            fpsCamera.transform.eulerAngles =
+                fpsCamera.transform.eulerAngles +
+                new Vector3(mouseY * (IsInvertYaxis ? 1 : -1), 0, 0); // 조건식을 3항식으로 대체했음.
+
+            var keyboardX = Input.GetAxis("Horizontal"); // ws, 좌 우 버튼을 누르면 값을 받음
+            var keyboardY = Input.GetAxis("Vertical"); // sw, 위 아래 버튼을 누르면 값을 받음
+
+            // 앞으로 나가아는 속도는 KeyboardX의 값으로 한다. // Left Shift를 누를 경우 달린다.
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                movementSpeed = 6;
+                // print(movementSpeed);
+            }
+            else
+            {
+                movementSpeed = 3;
+                // print(movementSpeed);
+            }
+
+            fpsRigidBody.velocity = (transform.forward * (keyboardY * movementSpeed)) +
+                                    (transform.right * (keyboardX * movementSpeed));
+        }
     }
 }
