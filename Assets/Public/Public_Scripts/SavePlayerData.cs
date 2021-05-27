556d3c967cb284534ef4ff34f8c4cbf62d53cf4d
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2021-05-28 03:52:18 +0900

    210528_MainMenu_Update
    1. 타이틀 3D 배경 터레인 배치
    2. 타이틀 조명 설정 및 배치
    3. 타이틀 3D 캐릭터 배치
    4. 타이틀 음원 삽입 등
    Public 폴더를 추가하여 sound effect 등의 자료를 추가함.

---
+++ Assets/Public/Public_Scripts/SavePlayerData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SavePlayerData
+{
+    public Vector3 playerPosition;
+    public Vector3 playerRotationEuler;
+}
